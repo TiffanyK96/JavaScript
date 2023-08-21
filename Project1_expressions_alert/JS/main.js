0eabbf29445293cb947433647300e5e452d00771
TiffanyK96 <128201111+TiffanyK96@users.noreply.github.com>
2023-08-21 15:41:24 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+document.write('Welcome, traveller! '); 
+var A="This is a string. "; 
+document.write(A); 
+var B="This is my first challenge. "
+window.alert(B); 
+var C="This is another \"string\"! "
+document.write(C); 
+
+document.write("\"Here I am, "+ "once again, "+ "doing another assignment, "+"this time it\'s concatenating.\" - Tiffany ")
+
+var D="Concatenated String"+ " Challenge"
+document.write(D); 
+
+var Happiness="Warm Drinks", morning="water", afternoon="tea", evening="tisane"; 
+document.write(morning); 
+
+document.write(6+6); //Here is an expression. Hopefully it's not too simplistic! 
+
+//I really gotta add some line breaks and spacing 
+
+window.alert(Have a good day!); //This is me using window.alert. Up above you can see me using document.write. 
+
+var Sent1="This is the beginning of the string"; 
+var Sent2=" and this is the end of the string. ";  
+document.write(Sent1+Sent2); //Here are the 2 variables concatenated 
